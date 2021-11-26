b0a7c8fdab59dd3d40e85527a42b8734f04c6284
Paulo Steffen Machado <paulo.machado@cwi.com.br>
2021-11-26 18:24:47 -0300

    feat: refactir docker compose

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://54.94.88.227:3000/',
+  baseUrl: 'https://52.67.222.136:3000/',
   googleApi: 'https://www.googleapis.com/books/v1/volumes'
 };
