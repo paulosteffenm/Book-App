f45fd1949580a9e8d9f7cc81118d497d5e46b9fe
Paulo Steffen Machado <paulo.machado@cwi.com.br>
2021-12-01 22:07:14 -0300

    chore: cert

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://52.67.222.136:3000/',
+  baseUrl: 'http://52.67.222.136:3000/',
   googleApi: 'https://www.googleapis.com/books/v1/volumes'
 };
